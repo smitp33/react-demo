e6d16783eda4eb99cd297eaad6e0cf13ce713006
Philip J Smith <Philip.J.Smith@aexp.com>
2017-10-27 17:14:37 +0100

    game

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './css/index.css';
 import App1 from './App';
 import App2 from './Button'
 import {Button, Result} from './Button'
+import App3 from './Card'
+import App4 from './Game'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App1 />, document.getElementById('root'));
 ReactDOM.render(<App2 counter="2"/>, document.getElementById('a'));
 ReactDOM.render(<Button incrementValue="2" onClickFunction={this.handleClick}/>, document.getElementById('b'));
 ReactDOM.render(<Result counter="2"/>, document.getElementById('c'));
+
+ReactDOM.render(<App3 />, document.getElementById('d'));
+ReactDOM.render(<App4 />, document.getElementById('d'));
 registerServiceWorker();
