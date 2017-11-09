f630d7f935c7046676a0cd2d80e1b8cb7b3c0e9d
Philip J Smith <Philip.J.Smith@aexp.com>
2017-11-09 15:52:19 +0000

    latests

---
+++ src/Problem.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+class Parent extends Component {
+  state = {counter: 0}
+
+  onClick = () => {
+    console.log("Button clicked")
+    this.setState(prevState => ({
+      counter: prevState.counter + 1
+    }))
+  }
+
+  onClickAsync = () => {
+    console.log("Async Button clicked")
+    axios.get(`https://api.github.com/users/smitp33`)
+      .then(resp => {
+          this.setState(prevState => ({
+            counter: prevState.counter + 1
+          }))
+        }
+      )
+  }
+
+  render() {
+    return (
+      <div>
+        <Child onClick={this.onClick} action="Click"/>
+      </div>
+    );
+  }
+}
+
+class Child extends Component {
+
+  render() {
+    return (
+      <button onClick={this.props.onClickAsync}>{this.props.action}</button>
+    )
+  }
+}
+
+export default Parent
+
+
